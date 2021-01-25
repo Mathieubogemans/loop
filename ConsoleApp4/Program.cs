91ea4c2d3357c5ee6ed6831b44a697d29231c809
Mathieu Bogemans <mathieu.bogemans@student.romerocollege.be>
2021-01-26 00:53:07 +0100

    1.1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Wisselkoers 1.0
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,13 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"curent getal {i}");
-                i = i++;
-            }
+            Console.OutputEncoding = Encoding.UTF8;
+            decimal wisselkoers = 1.21m;
+
+            Console.WriteLine("geef euro waarde in ");
+            decimal geld = decimal.Parse( Console.ReadLine());
+            decimal nieuwgeld = geld * wisselkoers ;
+            Console.WriteLine($"u {geld}€ is {nieuwgeld} dolar ");
             Console.ReadLine();
         }
     }
